88762681de53dae601c55b7d1d1f81966d517c66
Williams FH <fwillimas27@uagrantham.edu>
2024-12-08 15:57:43 -0500

    Initial commit of Ionic project

---
+++ ios/App/Pods/Target Support Files/CordovaPlugins/CordovaPlugins-umbrella.h
@@ -0,0 +1,28 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "CDVCamera.h"
+#import "CDVExif.h"
+#import "CDVJpegHeaderWriter.h"
+#import "UIImage+CropScaleOrientation.h"
+#import "CDVDevice.h"
+#import "CDVLocation.h"
+#import "CDVInAppBrowserNavigationController.h"
+#import "CDVInAppBrowserOptions.h"
+#import "CDVWKInAppBrowser.h"
+#import "CDVWKInAppBrowserUIDelegate.h"
+#import "CDVConnection.h"
+#import "CDVReachability.h"
+
+FOUNDATION_EXPORT double CordovaPluginsVersionNumber;
+FOUNDATION_EXPORT const unsigned char CordovaPluginsVersionString[];
+
